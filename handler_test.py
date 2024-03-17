b9717112023c4da52e695b5c70f54c0b035c40ac
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-03-17 13:20:38 +0900

    add unit test

--- handler_test.py
+++ handler_test.py
@@ -1,5 +1,16 @@
 import pytest
 
-@pytest.mark.parametrize("num1,num2,expected", [(1, 2, 3), (4, 5, 9), (0, 1, 1)])
-def test_addition(num1, num2, expected):
-    assert num1 + num2 == expected
+import dpath
+
+from handler import get_monthly_cost
+
+def test_get_monthly_cost():
+    monthly_cost = get_monthly_cost(2024, 3)
+    assert isinstance(monthly_cost, dict)
+
+    results = dpath.get(monthly_cost, 'ResultsByTime')
+    assert isinstance(results, list)
+    assert len(results) == 1
+
+    total = dpath.get(results[0], 'Total/UnblendedCost/Amount')
+    assert total is not None
