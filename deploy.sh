dbccc6d68e70d160038409f8879330d20abebb8f
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-03-19 02:15:18 +0900

    fix deploy.sh and serverless.yml for stg

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+stage=$1
+
 if [ -d ".build" ]; then
 	rm -rf .build
 fi
 
 mkdir .build
-cp package.json package-lock.json .build
-cp *.py .build
-cp config.*.yml .build
-cp .env.prd .build
-cp requirements.txt .build
+mkdir .build/lib
 
-cd .build
+cp package.json package-lock.json .build
+cp -r src .build
+cp .env.${stage} .build/.env
+cp requirements.txt .build/lib
+
+if [ "$stage" = "local" ]; then
+	cp deploy/serverless.local.yml .build/serverless.yml
+else
+	cp deploy/serverless.yml .build
+fi
+
+cd .build/lib
 npm ci
 pip install -r requirements.txt -t .
 
-npx serverless deploy --stage prd
+cd ..
+
+npx serverless deploy --stage $stage --force
