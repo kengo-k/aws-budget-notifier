91081e6a9bff581995b5d1a5a219b2264de36e1e
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-03-19 20:33:47 +0900

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 stage=$1
 
 if [ -d ".build" ]; then
-	rm -rf .build
+  rm -rf .build
 fi
 
 mkdir .build
@@ -15,15 +15,18 @@ cp .env.${stage} .build/.env
 cp requirements.txt .build/lib
 
 if [ "$stage" = "local" ]; then
-	cp deploy/serverless.local.yml .build/serverless.yml
+  cp deploy/serverless.local.yml .build/serverless.yml
 else
-	cp deploy/serverless.yml .build
+  cp deploy/serverless.yml .build
 fi
 
 cd .build/lib
 npm ci
 pip install -r requirements.txt -t .
-
 cd ..
+
+set -a
+source .env
+set +a
 
 npx serverless deploy --stage $stage --force
