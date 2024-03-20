10822f1d08e039b919ed5d7fa8f08d6eecb25219
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-03-20 18:24:15 +0900

    add vscode settings and format code

--- src/handler.py
+++ src/handler.py
@@ -4,74 +4,76 @@ import json
 import requests
 import boto3
 
+
 def get_webhook_url():
-    key = 'BUDGET_NOTIFIER_SLACK_WEBHOOK_URL'
+    key = "BUDGET_NOTIFIER_SLACK_WEBHOOK_URL"
     webhook_url = os.environ.get(key)
     if webhook_url is not None:
         return webhook_url
-    
-    ssm_client = boto3.client('ssm')
+
+    ssm_client = boto3.client("ssm")
     response = ssm_client.get_parameter(Name=key, WithDecryption=True)
-    webhook_url = response['Parameter']['Value']
-    
+    webhook_url = response["Parameter"]["Value"]
+
     return webhook_url
 
+
 def get_monthly_cost(year, month):
-    ce = boto3.client('ce')
+    ce = boto3.client("ce")
     monthly_cost = ce.get_cost_and_usage(
         TimePeriod={
-            'Start': f'{year}-{month:02d}-01',
-            'End': f'{year}-{month+1:02d}-01' if month < 12 else f'{year+1}-01-01'
+            "Start": f"{year}-{month:02d}-01",
+            "End": f"{year}-{month+1:02d}-01" if month < 12 else f"{year+1}-01-01",
         },
-        Granularity='MONTHLY',
-        Metrics=['UnblendedCost'],
-        GroupBy=[
-            {'Type': 'DIMENSION', 'Key': 'SERVICE'},
-            {'Type': 'DIMENSION', 'Key': 'RECORD_TYPE'}
-        ]
+        Granularity="MONTHLY",
+        Metrics=["UnblendedCost"],
+        GroupBy=[{"Type": "DIMENSION", "Key": "SERVICE"}, {"Type": "DIMENSION", "Key": "RECORD_TYPE"}],
     )
 
     result = []
     total_cost = 0
 
-    for item in monthly_cost['ResultsByTime'][0]['Groups']:
-        record_type = item['Keys'][1]
-        if record_type == 'Tax':
-            service_name = 'Tax'
+    for item in monthly_cost["ResultsByTime"][0]["Groups"]:
+        record_type = item["Keys"][1]
+        if record_type == "Tax":
+            service_name = "Tax"
         else:
-            service_name = item['Keys'][0]
-        
-        cost = float(item['Metrics']['UnblendedCost']['Amount'])
+            service_name = item["Keys"][0]
+
+        cost = float(item["Metrics"]["UnblendedCost"]["Amount"])
         result.append((service_name, cost))
         total_cost += cost
 
-    services = sorted([item for item in result if item[0] not in ['Tax', 'Total']], key=lambda x: x[1], reverse=True)
-    
-    tax_item = next((item for item in result if item[0] == 'Tax'), None)
-    total_item = ('Total', total_cost)
-    
+    services = sorted([item for item in result if item[0] not in ["Tax", "Total"]], key=lambda x: x[1], reverse=True)
+
+    tax_item = next((item for item in result if item[0] == "Tax"), None)
+    total_item = ("Total", total_cost)
+
     sorted_result = services + ([tax_item] if tax_item else []) + [total_item]
-    
+
     result_with_percentage = [(service_name, cost, cost / total_cost * 100) for service_name, cost in sorted_result]
     return result_with_percentage
+
 
 def get_report_string(cost_data):
     report = []
     for item in cost_data:
         service_name, cost, percentage = item
-        formatted_cost = f'${cost:.2f}'
-        formatted_percentage = f'({percentage:.1f}%)'
-        report_line = f'{service_name}: {formatted_cost} {formatted_percentage}'
+        formatted_cost = f"${cost:.2f}"
+        formatted_percentage = f"({percentage:.1f}%)"
+        report_line = f"{service_name}: {formatted_cost} {formatted_percentage}"
         report.append(report_line)
-    
-    return '\n'.join(report)
+
+    return "\n".join(report)
+
 
 def notify_slack(message):
     webhook_url = get_webhook_url()
-    data = {'text': message}
+    data = {"text": message}
     response = requests.post(webhook_url, json=data)
-    
+
     return response
+
 
 def main(event, context):
 
@@ -84,19 +86,14 @@ def main(event, context):
 
     notify_slack(report_string)
 
-    body = {
-        "message": "success",
-        "data": monthly_cost
-    }
+    body = {"message": "success", "data": monthly_cost}
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     event = {}
     context = None
     main(event, context)
--- tests/handler_test.py
+++ tests/handler_test.py
@@ -1,7 +1,22 @@
+from unittest.mock import patch
+
 from handler import get_monthly_cost
 from handler import get_report_string
 
-def test_get_monthly_cost():
+mocked_value = [
+    ("AWS Lambda", 100.0, 50.0),
+    ("Amazon S3", 50.0, 25.0),
+    ("Amazon EC2", 30.0, 15.0),
+    ("Tax", 20.0, 10.0),
+    ("Total", 200.0, 100.0),
+]
+
+
+@patch("handler.get_monthly_cost")
+def test_get_monthly_cost(mock_get_monthly_cost):
+
+    mock_get_monthly_cost.return_value = mocked_value
+
     monthly_cost = get_monthly_cost(2024, 3)
     assert isinstance(monthly_cost, list)
     assert len(monthly_cost) > 0
@@ -18,7 +33,12 @@ def test_get_monthly_cost():
     assert isinstance(second, float)
     assert isinstance(third, float)
 
-def test_get_report_string():
+
+@patch("handler.get_monthly_cost")
+def test_get_report_string(mock_get_monthly_cost):
+
+    mock_get_monthly_cost.return_value = mocked_value
+
     monthly_cost = get_monthly_cost(2024, 3)
     report_string = get_report_string(monthly_cost)
 
@@ -31,4 +51,3 @@ def test_get_report_string():
 
     last1 = lines[-2]
     assert last1.startswith("Tax:")
-    
